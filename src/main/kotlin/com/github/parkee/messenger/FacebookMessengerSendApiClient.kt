0a53dc8aa35f43f758f1f57b55ee5ea4943a9245
Pavlo Klivak <pavlo_klivak@corside.com>
2016-05-05 14:59:08 +0300

    add some logging and fix:) issue with unwrapping facebook error by jackson

--- src/main/kotlin/com/github/parkee/messenger/FacebookMessengerSendApiClient.kt
+++ src/main/kotlin/com/github/parkee/messenger/FacebookMessengerSendApiClient.kt
@@ -1,5 +1,8 @@
 package com.github.parkee.messenger
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
@@ -14,19 +17,29 @@ import com.github.parkee.messenger.model.settings.ThreadState
 import com.github.parkee.messenger.model.user.FacebookUserProfile
 import com.github.parkee.messenger.util.json.fromJsonTo
 import com.github.parkee.messenger.util.json.toJsonAsBytes
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 /**
  * Created by parkee on 4/28/16.
  */
 class FacebookMessengerSendApiClient(private val pageAccessToken: String) {
 
+    private val LOG : Logger = LoggerFactory.getLogger(FacebookMessengerSendApiClient::class.java);
+
+    init {
+        jacksonObjectMapper().configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true)
+    }
+
     private val defaultHeader = mapOf("Content-Type" to "application/json")
 
     fun sendRequest(facebookRequest: FacebookRequest): SendMessageResponse {
-        val (success, error) = "https://graph.facebook.com/v2.6/me/messages?access_token=$pageAccessToken".httpPost()
+        val request = "https://graph.facebook.com/v2.6/me/messages?access_token=$pageAccessToken".httpPost()
                 .header(defaultHeader)
                 .body(facebookRequest.toJsonAsBytes())
-                .responseString().third
+
+        LOG.debug(request.cUrlString())
+        val (success, error) = request.responseString().third
 
         checkForErrors(success, error)
         return success!!.fromJsonTo(SendMessageResponse::class)
@@ -34,10 +47,12 @@ class FacebookMessengerSendApiClient(private val pageAccessToken: String) {
 
     fun getUserProfile(userId: Long,
                        fields: List<String> = listOf("first_name", "last_name", "profile_pic")): FacebookUserProfile {
-        val (success, error) = "https://graph.facebook.com/v2.6/$userId".httpGet(listOf(
+        val request = "https://graph.facebook.com/v2.6/$userId".httpGet(listOf(
                 "access_token" to pageAccessToken,
                 "fields" to fields.joinToString(",")))
-                .responseString().third
+
+        LOG.debug(request.cUrlString())
+        val (success, error) = request.responseString().third
 
         checkForErrors(success, error)
         return success!!.fromJsonTo(FacebookUserProfile::class)
@@ -47,11 +62,13 @@ class FacebookMessengerSendApiClient(private val pageAccessToken: String) {
 
         val threadSettings = ThreadSettings(SettingType.CALL_TO_ACTIONS, ThreadState.NEW_THREAD, listOf(mapOf("message" to message)))
 
-        val (success, error) = "https://graph.facebook.com/v2.6/$pageId/thread_settings?access_token=$pageAccessToken"
+        val request = "https://graph.facebook.com/v2.6/$pageId/thread_settings?access_token=$pageAccessToken"
                 .httpPost()
                 .header(defaultHeader)
                 .body(threadSettings.toJsonAsBytes())
-                .responseString().third
+
+        LOG.debug(request.cUrlString())
+        val (success, error) = request.responseString().third
 
         checkForErrors(success, error)
         return success.orEmpty()
@@ -61,7 +78,10 @@ class FacebookMessengerSendApiClient(private val pageAccessToken: String) {
         val errorData = error?.errorData
         if (errorData != null || success == null) {
             val facebookErrorData = errorData ?: "{}".toByteArray()
-            throw FacebookException(facebookErrorData.fromJsonTo(FacebookError::class))
+            LOG.error(String(facebookErrorData))
+            val tr = object : TypeReference<Map<String, FacebookError>>() {}
+            val facebookError = facebookErrorData.fromJsonTo(tr)["error"] ?: throw FacebookException()
+            throw FacebookException(facebookError)
         }
     }
 }
--- src/main/kotlin/com/github/parkee/messenger/model/error/FacebookError.kt
+++ src/main/kotlin/com/github/parkee/messenger/model/error/FacebookError.kt
@@ -2,10 +2,8 @@ package com.github.parkee.messenger.model.error
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.annotation.JsonRootName
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonRootName("error")
 data class FacebookError(
         @JsonProperty("message") val message: String,
         @JsonProperty("type") val type: String,
